333c2793152536a5cb58e6fbe82e6e567ebd3c27
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-10-29 01:21:25 +0500

    Added Automapper profiles utility.

---
+++ Health-Watch-Utility/Utilities/AutopMapperProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_Utility.Utilities
+{
+    public class AutopMapperProfiles
+    {
+        public class AutoMapperProfile : Profile
+        {
+            public AutoMapperProfile()
+            {
+                //User Mappings
+
+                //Patient Mappings
+
+                //Disease Mappings
+            }
+        }
+    }
+}
