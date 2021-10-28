90dd77f694e8feaf99af8684349751eeb3c349ff
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-10-29 01:19:52 +0500

    Added Dependency Inversion control to handle dependencies.

---
+++ Health-Watch-IoC/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_IoC
+{
+    public class DependencyInjection
+    {
+        private readonly IConfiguration _configuration;
+        public DependencyInjection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void InjectDependencies(IServiceCollection services)
+        {
+            //Add DbContext service
+
+            //Add AutoMapper service
+
+            //Add other transient,scoped,singleton services here
+        }
+    }
+}
