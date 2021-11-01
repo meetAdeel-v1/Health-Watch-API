a81a95acc59a50a81708c6f9891c0af2a8ef8634
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-01 23:55:29 +0500

    Added patient entity.

---
+++ Health-Watch-Entity/Entities/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_Entity.Entities
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int MobileNo { get; set; }
+        //Address based on Province,District,City
+    }
+}
