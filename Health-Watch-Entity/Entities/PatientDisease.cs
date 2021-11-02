ee2b5679a33b050a034a93658f94b3a48015ee33
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-02 23:52:18 +0500

    Added refernce entity.

---
+++ Health-Watch-Entity/Entities/PatientDisease.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_Entity.Entities
+{
+    public class PatientDisease
+    {
+        [Key]
+        public int Id { get; set; }
+        public Patient Patient { get; set; }
+        [ForeignKey("")]
+        public int PatientId { get; set; }
+        public Disease Disease { get; set; }
+        [ForeignKey("")]
+        public int DiseaseId { get; set; }
+    }
+}
