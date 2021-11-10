ad48b3b17d477b62267d722b1099bc81d0c13507
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-11 03:17:45 +0500

    Added Patient Entity DTOs.

---
+++ Health-Watch-DTO/DTOs/Patient/PatientToListDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_DTO.DTOs.Patient
+{
+    public class PatientToListDTO
+    {
+
+    }
+}
