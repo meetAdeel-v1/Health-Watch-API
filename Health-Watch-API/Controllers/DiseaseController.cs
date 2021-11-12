d0fc5ac140b31ca0b7482ef39f1c7fcb4f905212
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-13 00:48:02 +0500

    Added Account Controller.

---
+++ Health-Watch-API/Controllers/DiseaseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Health_Watch_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiseaseController : ControllerBase
+    {
+        public DiseaseController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(int user)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(string user)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string user)
+        {
+            return null;
+        }
+    }
+}
