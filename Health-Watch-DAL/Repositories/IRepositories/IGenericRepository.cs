3ecccb582d0d438afa84f53a25c735d55bb952a3
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-17 03:25:25 +0500

    Added Generic Repo  dependency.

---
+++ Health-Watch-DAL/Repositories/IRepositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_DAL.Repositories.IRepositories
+{
+    public interface IGenericRepository
+    {
+
+    }
+}
