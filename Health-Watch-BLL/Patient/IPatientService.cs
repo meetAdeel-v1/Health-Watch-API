e9cdba48aeafe7f4f566e3c8ed13da69b5c661a3
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-19 00:02:15 +0500

    Added Pateint Service.

---
+++ Health-Watch-BLL/Patient/IPatientService.cs
@@ -0,0 +1,18 @@
+﻿using Health_Watch_DTO.DTOs.Patient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_BLL.Patient
+{
+    public interface IPatientService
+    {
+        Task<PatientToListDTO> Get(int Id);
+        Task<List<PatientToListDTO>> GetAll();
+        Task<PatientToAddDTO> Add(PatientToAddDTO entity);
+        Task<PatientToUpdateDTO> Update(PatientToUpdateDTO entity);
+        Task<int> Delete(PatientToAddDTO entity);
+    }
+}
