c69fdd88d9550f4d93bfebf2090bf38d2fb3b555
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-23 03:29:46 +0500

    Added Disease Repo Dependency.

---
+++ Health-Watch-DAL/Repositories/IRepositories/IDiseaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Health_Watch_Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_DAL.Repositories.IRepos
+{
+    public interface IDiseaseRepository
+    {
+        Task<Disease> Get(int id);
+        Task<List<Disease>> GetAll();
+        Task<Disease> Add(Disease entity);
+        Task<Disease> Update(Disease entity);
+        Task<int> Delete(Disease entity);
+    }
+}
