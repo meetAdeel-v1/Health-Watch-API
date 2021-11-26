1fd3cdf1c3fd603dda7353bb4ff0e44bd61496e5
Adeel Ahmed <ahmedadeel390@gmail.com>
2021-11-26 23:53:58 +0500

    Added Patient Repo dependency.

---
+++ Health-Watch-DAL/Repositories/IRepositories/IPatientRepository.cs
@@ -0,0 +1,18 @@
+﻿using Health_Watch_Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_Watch_DAL.Repositories.IRepos
+{
+    public interface IPatientRepository
+    {
+        Task<Patient> Get(int id);
+        Task<List<Patient>> GetAll();
+        Task<Patient> Add(Patient entity);
+        Task<Patient> Update(Patient entity);
+        Task<int> Delete(Patient entity);
+    }
+}
